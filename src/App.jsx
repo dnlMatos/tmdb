1846151cfe8f11a1832e384c3cc6205de9b9accf
dnlMatos <georginedanilo@gmail.com>
2022-07-13 14:45:12 -0300

    contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Descricao from "./components/descricao/Descricao";
+import Filmes from "./components/filmes/Filmes";
 import Header from "./components/header/Header";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <Header/>
      <Descricao/>
+     <Filmes/>
     </div>
   );
 }
---
+++ src/context/globalContext.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { getFilms } from "../requests/request";
+import { Context } from "./context";
+
+export default function globalContext(props) {
+  const [filmList, setFilmList] = useState([]);
+
+  useEffect(() => {
+    getAllFilm();
+  }, []);
+
+  const getAllFilm = async () => {
+    const response = await getFilms();
+    setFilmList(response);
+  };
+  return (
+    <Context.Provider value={{ filmList, setFilmList }}>
+      {props.children}
+    </Context.Provider>
+  );
+}
