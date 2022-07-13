f3594a1b34f071dce9d9f8bdbab8d2c2c3173874
dnlMatos <georginedanilo@gmail.com>
2022-07-12 21:21:25 -0300

    changeButton

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Descricao from "./components/descricao/Descricao";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Descricao/>
+    </div>
+  );
+}
+
+export default App;
